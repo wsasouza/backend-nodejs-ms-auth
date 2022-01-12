d8f8d7db831454cda3a36dcf5d0683cdbab98841
wsasouza <wsasouza@hotmail.com>
2022-01-11 22:21:18 -0300

    Chore: add Express and configure ts-node-dev

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log('Executadu')
+import express, { NextFunction, Request, Response } from 'express';
+
+const app = express();
+
+app.get('/status', (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).send({ hello: 'wsasouza' });
+}); 
+
+app.listen(3000, () => {
+  console.log('Server is running...');
+})
